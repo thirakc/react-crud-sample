bb8bfc7bfe39fe17bbb2e565b5263b501140d22e
Thirak <chuayjan.thirak@gmail.com>
2021-08-08 17:37:10 +0700

    forget to bind function

--- src/components/create-product.component.js
+++ src/components/create-product.component.js
@@ -1,10 +1,59 @@
 import React, { Component } from 'react'
-
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 export default class CreateProduct extends Component {
+
+    constructor(props) {
+        super(props)
+        this.onChangeProductName = this.onChangeProductName.bind(this)
+        this.onChangeProductDescription = this.onChangeProductDescription.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+
+        this.state = {
+            name: "",
+            description: ""
+        }
+    }
+
+    onChangeProductName(e) {
+        this.setState({ name: e.target.value })
+    }
+
+    onChangeProductDescription(e) {
+        this.setState({ description: e.target.value })
+    }
+
+    onSubmit(e) {
+        e.preventDefault()
+
+        console.log("student successfully created!")
+        console.log(`Name: ${this.state.name}`)
+        console.log(`Description: ${this.state.description}`)
+
+        this.setState({
+            name: "",
+            description: "",
+        })
+    }
+
     render() {
         return (
-            <div>
-                <p>React Create Product Component!</p>
+            <div className="form-wrapper mt-5">
+                <h1>Create Product</h1>
+                <Form onSubmit={this.onSubmit}>
+                    <Form.Group controlId="Name">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="text" value={this.state.name} onChange={this.onChangeProductName}/>
+                    </Form.Group>
+                    <Form.Group controlId="Description">
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control type="text" value={this.state.description} onChange={this.onChangeProductDescription}/>
+                    </Form.Group>
+
+                    <Button variant="success" size="lg" block="block" type="submit">
+                        Create Product
+                    </Button>
+                </Form>
             </div>
         )
     }
